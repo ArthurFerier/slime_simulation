e589c2618cf1330a49bdf5e7848aa574300921bd
Arthur <arthur.ferier1@gmail.com>
2023-01-06 23:13:37 +0100

    slimes moves upward and have trails now

--- Assets/Scripts/SlimeBehavior.cs
+++ Assets/Scripts/SlimeBehavior.cs
@@ -13,6 +13,6 @@ public class SlimeBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0, 0.001f, 0);
+        transform.Translate(0, 0.003f, 0);
     }
 }
